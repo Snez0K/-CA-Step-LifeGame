f40a4c2fb333e193559eeb17358504d3bccfcba6
Snez0 <Snez0@DESKTOP-RON656Q>
2018-05-16 17:08:37 +0300

    Fixed #4

--- LifeGame/Program.cs
+++ LifeGame/Program.cs
@@ -6,7 +6,7 @@ namespace LifeGame
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var game = new Universe();
             game.tempgenerate();
